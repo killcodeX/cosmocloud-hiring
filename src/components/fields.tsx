c42c6e2a73c917b9ea918aa733d9dba4035d5048
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-17 20:49:17 +0530

    input and select change function done

--- src/components/fields.tsx
+++ src/components/fields.tsx
@@ -14,21 +14,43 @@ type Iprops = {
 let options = ["OBJECT", "STRING", "NUMBER", "BOOLEAN"];
 
 export default function Fields({ field, setField }: Iprops) {
+  // This function helps to update Input the field
+  const handleInput = (e: any, id: number) => {
+    let objIndex = field.findIndex((obj) => obj.id === id);
+    field[objIndex].name = e.target.value;
+    setField([...field]);
+  };
+
+  // This function helps to update select the field
+  const handleSelect = (e: any, id: number) => {
+    let objIndex = field.findIndex((obj) => obj.id === id);
+    field[objIndex].type = e.target.value;
+    setField([...field]);
+  };
+
+  // This function helps to delete the field
   const handleDelete = (id: number) => {
-    let newFields = field.filter(field => field.id !== id)
-    setField(newFields)
+    let newFields = field.filter((field) => field.id !== id);
+    setField(newFields);
   };
   return (
     <div className="fields">
       {field.map((item: any, index) => {
-        console.log(item)
+        console.log(item);
         return (
           <div key={item.id} className="field">
-            <div className="field-id">{index+1}.</div>
+            <div className="field-id">{index + 1}.</div>
             <div className="field-details">
               <div className="field-details-left">
-                <input value={field[index].name} />
-                <select>
+                <input
+                  type="text"
+                  value={field[index].name}
+                  onChange={(e) => handleInput(e, item.id)}
+                />
+                <select
+                  value={field[index].type}
+                  onChange={(e) => handleSelect(e, item.id)}
+                >
                   {options.map((item, index) => {
                     return (
                       <option key={index} value={item}>
