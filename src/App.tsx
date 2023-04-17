fa8ca10d68c6601965473929742fd85ddb7dc01d
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-17 18:50:36 +0530

    added typescript types to usestates

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Fields from "./components/fields";
 
+type fieldProps = {
+  id: number;
+  name: string;
+  type:string;
+}
+
 function App() {
-  return <div className="card">
-    <div className="card-header">
-      <div className="card-title">Field name and type</div>
+  const [field, setField] = useState<fieldProps[]>([
+    {
+      id: 1,
+      name: "person",
+      type: "OBJECT",
+    },
+  ]);
+  return (
+    <div className="card">
+      <div className="card-header">
+        <div className="card-title">Field name and type</div>
+        <div className="card-toolbar" role="button">
+          <i className="ri-add-circle-line"></i>
+        </div>
+      </div>
+      <div className="card-body">
+        <Fields field={field} setField={setField} />
+      </div>
     </div>
-    <div className="card-body">
-      <Fields/>
-    </div>
-  </div>;
+  );
 }
 
 export default App;
--- src/components/fields.tsx
+++ src/components/fields.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
+type fieldProps = {
+    id: number;
+    name: string;
+    type:string;
+}
+
+type Iprops = {
+    field: fieldProps[];
+    setField: (value: fieldProps[]) => void;
+}
+
 let options = ["OBJECT", "STRING", "NUMBER", "BOOLEAN"];
 
-export default function Fields() {
-  const [field, setField] = useState([
-    {
-      id: 1,
-      name: "person",
-      type: "OBJECT",
-    },
-  ]);
+export default function Fields({field, setField}:Iprops) {
+  
   return (
     <div className="fields">
-      {field.map((item) => {
+      {field.map((item:any) => {
         return (
           <div key={item.id} className="field">
             <div className="field-id">{item.id}.</div>
