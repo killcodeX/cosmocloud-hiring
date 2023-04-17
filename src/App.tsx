9c3a4707da916839b18755273801de226bafc6d2
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-17 17:45:46 +0530

    added card header and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="card">
+    <div className="card-header">
+      <div className="card-title">Field name and type</div>
     </div>
-  );
+    <div className="card-body">hello world</div>
+  </div>;
 }
 
 export default App;
